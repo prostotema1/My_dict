45729a54394fe2d33347e40e083761e112cbb634
prostotema1 <90394799+prostotema1@users.noreply.github.com>
2022-04-20 19:14:06 +0300

    Update exception.hpp

--- exception.hpp
+++ exception.hpp
@@ -1,13 +1,18 @@
+#pragma once
 #include"not_found_exception.hpp"
 template <class Key>
 class KeyNotFoundException : public not_found_exception<Key>
 {
 public:
-    explicit KeyNotFoundException(Key key);
+    explicit KeyNotFoundException(Key key) : key(std :: move(key)) {}
 
-    const Key& get_key() const noexcept override;
+    const Key& get_key() const noexcept override {
+        return key;
+    }
 
-    const char* what() const noexcept override;
+    const char* what() const noexcept override {
+        return "Key was not found in dictionary.";
+    }
 private:
     Key key;
-}
+};
