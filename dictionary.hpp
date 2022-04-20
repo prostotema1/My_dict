b5f36c633a50df530dbb6ddfc64542208ddd9088
prostotema1 <90394799+prostotema1@users.noreply.github.com>
2022-04-20 19:15:29 +0300

    Update dictionary.hpp

--- dictionary.hpp
+++ dictionary.hpp
@@ -9,4 +9,4 @@ public:
     virtual const Value& get(const Key& key) const = 0;
     virtual void set(const Key& key, const Value& value) = 0;
     virtual bool is_set(const Key& key) const = 0;
-}
+};
