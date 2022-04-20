5f8cd4b1909f50501f5b747644a5f8fbe7baafe7
prostotema1 <90394799+prostotema1@users.noreply.github.com>
2022-04-20 19:19:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,25 @@
- #include<iostream>
-#include"my_dict.hpp"
-#include "exception.hpp"
-#include <string>
+#include "my_dict.hpp"
+#include<iostream>
+#include<string>
 
-using std :: string;
-using std :: cout;
+using std::string;
+using std::cout;
+
 
 int main() {
-    try{
-        my_dict<int,string> dict;
-        dict.set(0,"Zero");
-        dict.set(1,"One");
-        dict.set(2,"Two");
-        cout << dict.is_set(0) << " " << dict.get(0) << std :: endl;
-        cout << dict.is_set(1) << " " << dict.get(1) << std :: endl;
-        cout << dict.is_set(3) << " " << dict.get(3) << std :: endl;
+    try {
+        my_dict<int, string> dict;
+        dict.set(0, "Zero");
+        dict.set(1, "One");
+        dict.set(2, "Two");
+        cout << dict.is_set(0) << " " << dict.get(0) << std::endl;
+        dict.set(0, "NoZero");
+        cout << dict.is_set(0) << " " << dict.get(0) << std::endl;
+        cout << dict.is_set(1) << " " << dict.get(1) << std::endl;
+        cout << dict.is_set(3) << " " << dict.get(3) << std::endl;
     }
-    catch(KeyNotFoundException <int> & ex){
-        std :: cerr << ex.what() << std :: endl << ex.GetKey();
+    catch (KeyNotFoundException <int>& ex) {
+        std::cerr << ex.what() << " "  << ex.get_key();
     }
     return 0;
 }
